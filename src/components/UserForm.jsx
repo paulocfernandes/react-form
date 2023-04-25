31b5a1e29e4271fe82060c3a44f4aeada532e1f3
mcardoso10 <marcelosousacardoso@gmail.com>
2023-04-24 21:27:42 -0300

    inserindo onchange

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,29 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Labelform } from './labelform'
 
 
-export const UserForm = () => {
+export const UserForm = (event) => {
+
+    const [nome, Setnome] = useState ();
+    const [email, Setemail] = useState();
+    const [senha, Setsenha] = useState ();
+   
+    const submitForm = () => {
+        event.preventDefault();
+        console.log("submit");
+    }
+
+
+  
+
   return (
+
     <div className='container'>
+        
         <h1>Cadastro de usuário</h1>
-        <form>
+        
+        <form onSubmit={'submitForm'}>
 
-            <div className='col-md-4 mb-3'>
-                
+            <div className='col-md-4 mb-3'>  
             <Labelform  name='Nome'/>
-            <input type="text" name='nome' id='nome' className='form-control'       />
-
+            <input type="text"
+             name='nome' id='nome' 
+             className='form-control'
+             onChange={e => Setnome(e.target.value)}
+             value={nome}  
+             />
             </div>
 
             <div className='col-md-4 mb-3'>
     
             <Labelform name='Email' />
-            <input type="text" name='email' id='email' placeholder='Ex: nome@gmail.com' className='form-control'/>
+            <input type="text"
+             name='email' id='email'
+              placeholder='Ex: nome@gmail.com'
+               className='form-control'
+                value={email} 
+                onChange={e => Setemail(e.target.value)}/>
+               
             </div>
 
             <div className='col-md-4 mb-3'>
             <Labelform  name='Senha'/>
-            <input type="text" name='senha' id='senha'className='form-control'/>
+            <input type="text"
+             name='senha' id='senha'
+             className='form-control'  
+              valeu={senha}
+              onChange={e => Setsenha(e.target.value)} />
+           
             </div>
         
          
